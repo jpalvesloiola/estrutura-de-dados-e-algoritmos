227f53d87d59f95f33ac93e0862e2cfb8f71153a
jpalvesloiola <39224574+jpalvesloiola@users.noreply.github.com>
2022-10-29 01:31:17 -0300

    Cria Aula02 a Aula07

--- src/com/loiane/estruturadados/vetor/Vetor.java
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -19,7 +19,11 @@ public class Vetor {
 //			}
 //		}
 //	}
-
+	
+	/**
+	 * Adiciona elemento ao final do vetor
+	 * @param elemento
+	 */
 	public void adiciona(String elemento) {
 		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
@@ -30,12 +34,72 @@ public class Vetor {
 		
 	}
 	
+	/**
+	 * Adiciona elemento em qualquer posição do vetor.
+	 * Sobrecarga do metodo anterior.
+	 * @param elemento
+	 * @param posicao
+	 */
+	public void adiciona(String elemento, int posicao) {
+		
+		if (posicao < 0 || posicao > this.tamanho) {
+			throw new IllegalArgumentException("Posicao invalida!");
+		}
+		
+		for (int i = this.tamanho - 1; i >= posicao; i--) {
+			this.elementos[i+1] = this.elementos[i];
+		}
+		
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+		
+	}
+	
+	private void aumentaCapacidade() {
+		String [] novaCapacidade = String[this.elementos.length * 2];
+		for (int i = 0; i < novaCapacidade.length; i++) {
+			
+		}
+	}
+	
+	/**
+	 * Obtem elemento em uma posição do vetor
+	 * @param posicao
+	 * @return elemento
+	 */
+	public String busca(int posicao) {
+		if (posicao < 0 || posicao > this.tamanho) {
+			throw new IllegalArgumentException("Posicao invalida!");
+		}
+		return this.elementos[posicao];
+	}
+	
+	/**
+	 * Verifica se elemento existe no vetor e retorna a posicao.
+	 * Sbreescrita do metodo anterior.
+	 * Algoritmo de busca sequencial.
+	 * @param elemento
+	 * @return posicao ou -1 se nada for encontrado.
+	 */
+	public int busca(String elemento) {
+		
+		for (int i = 0; i < elementos.length; i++) {
+			if (elementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+
+	
+	
 	public int tamanho() {
 		return this.tamanho;
 	}
 
 	@Override
 	public String toString() {
-		return "Vetor [elementos=" + Arrays.toString(elementos) + ", tamanho=" + tamanho + "]";
+		return "Vetor elementos = " + Arrays.toString(elementos);
 	}
 }
