c06137e7e5ee40228195bc57ffe526dcd8eb6de4
jpalvesloiola <jpalvesloiola@gmail.com>
2024-03-26 17:19:22 -0300

    Adiciona tratamento a entrada de dados

--- src/com/loiane/estruturadados/pilha/labs/Exer01.java
+++ src/com/loiane/estruturadados/pilha/labs/Exer01.java
@@ -6,30 +6,30 @@ import com.loiane.estruturadados.pilha.Pilha;
 
 public class Exer01 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Pilha<Integer> pilha = new Pilha<>();
-        int numero;
-        
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Digite um número:");
-            numero = scan.nextInt();
-            if (numero % 2 == 0) {
-                pilha.empilha(numero);
-            } else if(pilha.estaVazia()){
-                System.out.println("Sua pilha está vazia!");
-            } else  {
-                System.out.println("Número ímpar! Sua pilha foi reduzida.");
-                pilha.desempilha();
-            }
-        }
+        try (Scanner scan = new Scanner(System.in)) {
+			Pilha<Integer> pilha = new Pilha<>();
+			int numero;
+			
+			for (int i = 0; i < 5; i++) {
+			    System.out.println("Digite um número:");
+			    numero = scan.nextInt();
+			    if (numero % 2 == 0) {
+			        pilha.empilha(numero);
+			    } else if(pilha.estaVazia()){
+			        System.out.println("Sua pilha está vazia!");
+			    } else  {
+			        System.out.println("Número ímpar! Sua pilha foi reduzida.");
+			        pilha.desempilha();
+			    }
+			}
 
-        System.out.println("\nSua pilha foi conculída: ");
-        System.out.println(pilha);
+			System.out.println("\nSua pilha foi conculída: ");
+			System.out.println(pilha);
 
-        while (!pilha.estaVazia()) {
-            System.out.println("Desempilhando... " + pilha);
-        }            
-        
+			while (!pilha.estaVazia()) {
+			    System.out.println("Desempilhando... " + pilha);
+			}
+		}
         System.out.println();
         System.out.println("Sua pilha foi esvaziada!");
         
--- src/com/loiane/estruturadados/pilha/labs/Exer02.java
+++ src/com/loiane/estruturadados/pilha/labs/Exer02.java
@@ -6,45 +6,45 @@ import com.loiane.estruturadados.pilha.Pilha;
 
 public class Exer02 {
     public static void main(String[] args) {
-        Pilha par = new Pilha<>();
-        Pilha impar = new Pilha<>();
+        Pilha<Integer> par = new Pilha<>();
+        Pilha<Integer> impar = new Pilha<>();
 
-        Scanner scan = new Scanner(System.in);
-        int numero;
+        try (Scanner scan = new Scanner(System.in)) {
+			int numero;
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Digite um número:");
-            numero = scan.nextInt();
-            if(numero == 0){
+			for (int i = 0; i < 10; i++) {
+			    System.out.println("Digite um número:");
+			    numero = scan.nextInt();
+			    if(numero == 0){
 
-                Integer desempilhado = (Integer) par.desempilha();
-                //pilha par
-                if( desempilhado == null) {
-                    System.out.println("Pilha Par está vazia!");
-                }else{
-                    System.out.println("\nDesempilha da pilha Par: " + desempilhado);
-                }
+			        Integer desempilhado = (Integer) par.desempilha();
+			        //pilha par
+			        if( desempilhado == null) {
+			            System.out.println("Pilha Par está vazia!");
+			        }else{
+			            System.out.println("\nDesempilha da pilha Par: " + desempilhado);
+			        }
 
-                desempilhado = (Integer) impar.desempilha();
-                //pilha impar
-                if(desempilhado == null) {
-                    System.out.println("Pilha Impar está vazia!");
-                }else{ 
-                    System.out.println("Desempilha da pilha ìmpar: " + desempilhado);
-                }
-                
-                
-            }else if (numero % 2 == 0) {
-                System.out.println("Empilha número par, segue a pilha:");
-                par.empilha(numero);
-                
-            } else{                
-                System.out.println("Empilha número ímpar, segue a pilha:");
-                impar.empilha(numero);
-               
-            } 
-        }
-
+			        desempilhado = (Integer) impar.desempilha();
+			        //pilha impar
+			        if(desempilhado == null) {
+			            System.out.println("Pilha Impar está vazia!");
+			        }else{ 
+			            System.out.println("Desempilha da pilha ìmpar: " + desempilhado);
+			        }
+			        
+			        
+			    }else if (numero % 2 == 0) {
+			        System.out.println("Empilha número par, segue a pilha:");
+			        par.empilha(numero);
+			        
+			    } else{                
+			        System.out.println("Empilha número ímpar, segue a pilha:");
+			        impar.empilha(numero);
+			       
+			    } 
+			}
+		}
         System.out.println("\n\nPilha Par: \n" + par);
         System.out.println("Pilha Impar: \n" + impar);
 
